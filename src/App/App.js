3b675ce33eb5b7464081f67659d6f93e146261b9
Josh Aragon <josharagon99@gmail.com>
2021-03-30 12:03:03 -0600

    Add working puppy and shark links

--- src/App/App.js
+++ src/App/App.js
@@ -4,16 +4,42 @@ import puppies from '../data/puppy-data.js';
 import sharks from '../data/shark-data.js';
 import Creatures from '../Creatures/Creatures';
 import Home from '../Home/Home';
+import {
+  BrowserRouter,
+  Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import CreatureDetails from '../Creatures/CreatureDetails';
+
 
 export default class App extends Component {
+  findCreature(type, id) {
+    const animals = type === 'puppies' ? puppies : sharks;
+    const creatureToRender = animals.find(creature => creature.id === parseInt(id))
+    return creatureToRender;
+  }
   render() {
     return (
       <main className="App">
         <nav>
-          <a href="/puppies" className="nav">Puppies</a>
+          <Link to="/puppies" className="nav">Puppies</Link>
           <a href="/sharks" className="nav">Sharks</a>
         </nav>
         <h1>Puppies or Sharks?</h1>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/puppies" render={() => <Creatures name="puppies" data={puppies} />} />
+        <Route exact path="/sharks" render={() => <Creatures name="sharks" data={sharks} />} />
+        <Route
+          exact path='/:type/:id'
+          render={({ match }) => {
+            const { id, type } = match.params;
+            return (
+              <CreatureDetails {...this.findCreature(type, id)} />
+            )
+          }}
+        />
       </main>
     );
   }
--- src/Creatures/Creatures.js
+++ src/Creatures/Creatures.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './image-display.css';
+import { Link } from 'react-router-dom'
 
 const Creatures = ({ data, name}) => {
   const creatureImages = data.map(creature => {
-    const { id, image } = creature;
-    return <img src={image} key={id} id={id} className="app-img"/>
+    const { id, image, type } = creature;
+    return ( <Link to={`/${type}/${id}`}> <img src={image} key={id} id={id} className="app-img"/> </Link>)
   });
   return (
     <>
